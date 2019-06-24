b6e1d1ebb567ee3664975de0c32c98b80fc4c3db
admin <k.kubo.private.mobile@gmail.com>
2019-06-25 04:06:35 +0900

    create directive

--- projects/ace-module/src/lib/ace.directive.ts
+++ projects/ace-module/src/lib/ace.directive.ts
@@ -1,13 +1,75 @@
-import { Directive } from '@angular/core';
-
+import { Directive, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import * as AceAjax from 'brace';
+import { Editor } from 'brace';
 
 const brace = require('brace');
+
 @Directive({
   selector: '[ace]'
 })
-export class AceDirective {
+export class AceDirective implements OnInit {
+  @Input() mode = 'javascript';
+  @Input() theme = 'monokai';
+  @Input() value: string;
+  @Output() change = new EventEmitter();
 
-  constructor() {
+
+  private editor: Editor;
+
+  constructor(
+    private el: ElementRef
+  ) {
+  }
+
+  setValue(val: string, cursorPos?: number) {
+    this.editor.setValue(val, cursorPos);
+  }
+
+  getValue(): string {
+    return this.editor.getValue();
+  }
+
+  selection(): AceAjax.Selection {
+    return this.editor.selection;
+  }
+
+  moveCursorTo(row, column): void {
+    return this.editor.moveCursorTo(row, column);
+  }
+
+  clearSelection(): void {
+    return this.editor.clearSelection();
+  }
+
+  ngOnInit() {
+    this.initEditor();
+    this.setMode();
+    this.setTheme();
+    this.setValue(this.value);
+    this.addChange();
+    this.clearSelection();
+  }
+
+
+  private addChange() {
+    this.editor.getSession().on('change', () => {
+      this.change.emit(this.getValue());
+    });
+  }
+
+
+  private initEditor(): void {
+    this.editor = brace.edit(this.el.nativeElement);
+  }
+
+  private setMode(): void {
+    require(`brace/mode/${ this.mode }`);
+    this.editor.getSession().setMode(`ace/mode/${ this.mode }`);
+  }
+
+  private setTheme(): void {
+    require(`brace/theme/${ this.theme }`);
+    this.editor.setTheme(`ace/theme/${ this.theme }`);
   }
 
 }
--- projects/ace-module/src/lib/ace.module.ts
+++ projects/ace-module/src/lib/ace.module.ts
@@ -3,9 +3,9 @@ import { AceModuleComponent } from './ace-module.component';
 import { AceDirective } from './ace.directive';
 
 @NgModule({
-  declarations: [AceModuleComponent, AceDirective],
-  imports: [
-  ],
-  exports: [AceModuleComponent]
+  declarations: [ AceModuleComponent, AceDirective ],
+  imports: [],
+  exports: [ AceModuleComponent, AceDirective ]
 })
-export class AceModule { }
+export class AceModule {
+}
