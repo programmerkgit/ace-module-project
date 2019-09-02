899a2f68890fa8b11643e1b8bd1a62dd7a43cb3f
admin <k.kubo.private.mobile@gmail.com>
2019-09-03 00:19:57 +0900

    a

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AceComponent } from '../../../projects/ace-module/src/lib/ace.component';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: [ './test.component.scss' ]
 })
-export class TestComponent implements OnInit {
+export class TestComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(AceComponent, {static: true}) ace;
 
   terminalText = `
   a
@@ -26,6 +29,12 @@ export class TestComponent implements OnInit {
   `;
 
   constructor() {
+  }
+
+  ngAfterViewInit(): void {
+    setInterval(() => {
+      this.ace.setValue('fajweoi' + Math.random());
+    }, 100);
   }
 
   ngOnInit() {
